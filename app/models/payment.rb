553d46325875e8a3341b85ba9e2bc27f3c36f3c5
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-01-16 22:36:57 +0500

    establish model associations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :category_payments, dependent: :destroy
+  has_many :categories, through: :category_payments
 end
