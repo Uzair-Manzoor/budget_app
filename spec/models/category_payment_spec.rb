59155d54f109f035aa456b75841bcd7df3362c9b
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-01-17 23:44:01 +0500

    write spec for category_payment model

--- spec/models/category_payment_spec.rb
+++ spec/models/category_payment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CategoryPayment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to a category' do
+      association = described_class.reflect_on_association(:category)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a payment' do
+      association = described_class.reflect_on_association(:payment)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
 end
